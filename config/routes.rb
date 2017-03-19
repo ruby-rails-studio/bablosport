4071efcc21006c1b2dbf222f9376d4a7aeda4355
Ben <=>
2017-03-20 03:52:39 +1000

    test subdomains

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,24 @@ Rails.application.routes.draw do
   get 'about'    => 'site#about'
   get 'i/:id'    => 'items#show', as: :short_link
 
-  get 'lp/:id'   => 'landings#show'
+  # get 'lp/:id'   => 'landings#show'
 
-  get 'r', to: 'site#rating', constraints: { subdomain: 'admin' }
+  # get '/', to: 'landings#show', constraints: { subdomain: ':id' }
+
+class Subdomain
+  def self.matches?(request)
+    request.subdomain.present? && request.subdomain != 'www'
+  end
+end
+
+  constraints(Subdomain) do  
+    resource :landing, path: "/:id" do
+      member do
+        get '/lp'   => 'landings#show'
+      end
+    end
+  end
+
 
   resources :items
   resources :categories
