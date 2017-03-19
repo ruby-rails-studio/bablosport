252fb31632688c94b4cee02fc82c184997311123
Ben <=>
2017-03-20 04:02:49 +1000

    sitemap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,38 +1,8 @@
 # config valid only for current version of Capistrano
 lock "3.8.0"
 
-# set :application, "my_app_name"
-# set :repo_url, "git@example.com:me/my_repo.git"
-
-# # Default branch is :master
-# # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# # Default deploy_to directory is /var/www/my_app_name
-# # set :deploy_to, "/var/www/my_app_name"
-
-# # Default value for :format is :airbrussh.
-# # set :format, :airbrussh
-
-# # You can configure the Airbrussh format using :format_options.
-# # These are the defaults.
-# # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# # Default value for :pty is false
-# # set :pty, true
-
-# # Default value for :linked_files is []
-# # append :linked_files, "config/database.yml", "config/secrets.yml"
-
-# Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
-# # Default value for default_env is {}
-# # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# # Default value for keep_releases is 5
-# # set :keep_releases, 5
-
-# Change these
 server '195.161.114.138', roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:ruby-rails-studio/bablosport.git'
@@ -57,17 +27,6 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
-## Defaults:
-# set :scm,           :git
-# set :branch,        :master
-# set :format,        :pretty
-# set :log_level,     :debug
-# set :keep_releases, 5
-
-## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
@@ -81,8 +40,6 @@ namespace :puma do
 end
 
 namespace :deploy do
-
-  desc "reload the database with seed data"
 
 
   desc "Make sure local git is in sync with remote."
