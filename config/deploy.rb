995dd9f5a8cb6fd7fac28b89374f585259ea9a4f
Ben <=>
2017-03-20 01:59:27 +1000

    seed

--- config/deploy.rb
+++ config/deploy.rb
@@ -81,6 +81,12 @@ namespace :puma do
 end
 
 namespace :deploy do
+
+  desc "reload the database with seed data"
+  task :seed do
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+  end
+  
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
