8dc905f53e9e8a22a0f839ace588f980a57ec007
Ben <=>
2017-03-20 01:55:12 +1000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,14 @@
-# categories = 
-#   Category.create([
-#   	{ name: 'Букмекерские конторы', root: true}, 
-#   	{ name: 'Покер-румы',  root: true }
-#   ])
+Category.delete_all
+Category.create!([
+  	{ name: 'Букмекерские конторы', root: true}, 
+  	{ name: 'Покер-румы',  root: true }
+])
 
-
-items = 
-  Item.create([
+Item.delete_all
+Item.create!([
   	{ name: 'Leon', category_id: 1, is_published: true},
   	{ name: 'Лига ставок', category_id: 1, is_published: true},
   	{ name: 'Winline', category_id: 1, is_published: true},
   	{ name: '1xСтавка', category_id: 1, is_published: true},
   	{ name: 'Фонбет', category_id: 1, is_published: true}
-  ])
+])
